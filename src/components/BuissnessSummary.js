72056874d83986002cd77d826cc34cbc5927c475
ShawonsGit <s.shawon0000@gmail.com>
2022-05-23 20:54:53 +0600

    buissness summary added

---
+++ src/components/BuissnessSummary.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './BuissnessSummary.css'
+import exportImg from '../assets/export.jpg'
+import happyClient from '../assets/Happy user giving positive review of online service.jpg'
+import review from '../assets/review.jpg'
+import countries from '../assets/countries.jpg'
+const BuissnessSummary = () => {
+    return (
+        <>
+               
+        <div class=" shadow-2xl mb-10 lg:mr-44 ml-24 w-52 lg:w-auto lg:ml-44 grid lg:grid-cols-4">
+                <div class="stat">
+                <img class="mask mask-decagon w-44" src={exportImg} alt=''/>
+                    <div class="stat-title">Deliveried</div>
+                    <div class="stat-value">89,400+</div>
+                    <div class="stat-desc">21% more than last month</div>
+                </div>
+                <div class="stat">
+                <img class="mask mask-decagon w-44" src={happyClient} alt=''/>
+                    <div class="stat-title">Happy client</div>
+                    <div class="stat-value">80000+</div>
+                    <div class="stat-desc">21% more than last month</div>
+                </div>
+                <div class="stat">
+                <img class="mask mask-decagon w-44" src={review} alt=''/>
+                    <div class="stat-title">Reviews</div>
+                    <div class="stat-value">50000+</div>
+                    <div class="stat-desc">21% more than last month</div>
+                </div>
+                <div class="stat">
+                <img class="mask mask-decagon w-44" src={countries} alt=''/>
+                    <div class="stat-title">Countires</div>
+                    <div class="stat-value">130+</div>
+                    <div class="stat-desc">21% more than last month</div>
+                </div>
+  
+        </div>
+        </>
+    );
+};
+
+export default BuissnessSummary;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import BuissnessSummary from '../components/BuissnessSummary';
 import ToolCard from '../components/ToolCard';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
-          <div className='grid lg:grid-cols-3 gap-5 mt-10 lg:ml-24 ml-16 '>
+          <div className='grid lg:grid-cols-3 gap-5 mt-10 lg:ml-24 ml-20 '>
           <ToolCard></ToolCard>
            <ToolCard></ToolCard>
            <ToolCard></ToolCard>
           </div>
+          <h1 className='text-3xl text-center mt-32 mb-10 font-bold'>Achivemnets</h1>
+         <BuissnessSummary></BuissnessSummary>
         </div>
     );
 };
