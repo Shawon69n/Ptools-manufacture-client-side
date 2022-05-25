7c8081b477982faa14ebfc3780252222b60eeccb
ShawonsGit <s.shawon0000@gmail.com>
2022-05-25 20:27:41 +0600

    add review section rating system done

--- src/pages/Dashboard/AddReview.js
+++ src/pages/Dashboard/AddReview.js
@@ -12,54 +12,62 @@ const AddReview = () => {
     const onSubmit = async data => {
         const image = data.img[0];
         const formData = new FormData();
-        formData.append('image',image)
+        formData.append('image', image)
         const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`
-        fetch(url,{
+        fetch(url, {
             method: 'POST',
             body: formData
         })
-        .then(res => res.json())
-        .then(result => {
-            if(result.success){
-                const img = result?.data?.url;
-                const reviewer = {
-                    name : data.name,
-                    address: data.address,
-                    review: data.review,
-                    rating: data.rating,
-                    img: img
-                }
-             
-                // sending review database 
-                fetch('http://localhost:5000/reviews',{
-                    method: 'POST',
-                    headers: {
-                        'content-type' : 'application/json',
-                    },
-                    body: JSON.stringify(reviewer)
-                })
-                .then(res => res.json())
-                .then(added => {
-                    console.log('yohhoo');
-                })
+            .then(res => res.json())
+            .then(result => {
+                if (result.success) {
+                    const img = result?.data?.url;
+                    const reviewer = {
+                        name: data.name,
+                        address: data.address,
+                        review: data.review,
+                        rating: data.rating,
+                        img: img
+                    }
 
-            }
-        })
-        
+                    // sending review database 
+                    fetch('http://localhost:5000/reviews', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                        },
+                        body: JSON.stringify(reviewer)
+                    })
+                        .then(res => res.json())
+                        .then(added => {
+                            console.log('yohhoo');
+                        })
+
+                }
+            })
+
     };
     return (
         <div>
-            <div class="hero  ">
+            <div>
+                <h1 className='text-center mb-10 font-mono font-extrabold lg:text-2xl'>Give your precious review to us</h1>
                 <div class="hero-content flex-col lg:flex-row-reverse">
                     <img src={reviewImg} class="max-w-sm rounded-lg  shadow-2xl" alt='' />
                     <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
 
-                        <input  value={user?.displayName} class="w-96 input input-bordered shadow-2xl" {...register("name")} />
-                        <input type="text" placeholder='Your location' class="w-96 input input-bordered shadow-2xl mt-2" {...register("address", { required: true })} required/>
+                        <input value={user?.displayName} class="w-96 input input-bordered shadow-2xl" {...register("name")} />
+                        <input type="text" placeholder='Your location' class="w-96 input input-bordered shadow-2xl mt-2" {...register("address", { required: true })} required />
                         <textarea placeholder='Write your review' className='textarea input-bordered shadow-2xl mt-2' {...register("review", { required: true })} />
 
-                        <input type="file"  class="w-96 input input-bordered shadow-2xl mt-2 p-2" {...register("img")} required/>
-                        <input placeholder='Ratings' type='number' class="w-96 input input-bordered shadow-2xl mt-2" {...register("rating")} required />
+                        <input type="file" class="w-96 input input-bordered shadow-2xl mt-2 p-2" {...register("img")} required />
+                        <select class="select select-bordered select-sm mt-2" {...register("rating")} required >
+                            <option disabled selected>Pick your rating</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
                         <input className='btn btn-primary mt-2' type="submit" value='Add review' />
                     </form>
                 </div>
