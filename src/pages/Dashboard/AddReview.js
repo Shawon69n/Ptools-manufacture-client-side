111d5b82629ce9da3e42f32d252e5471d662347c
ShawonsGit <s.shawon0000@gmail.com>
2022-05-25 18:24:06 +0600

    add review data loaded from db

--- src/pages/Dashboard/AddReview.js
+++ src/pages/Dashboard/AddReview.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div>
-            <h2>add a review here</h2>
+            <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+
+                <input type="text"  value={user?.displayName} class="input input-bordered input-info w-full max-w-xs" {...register("Name")} />
+                <textarea placeholder='Write your review' className='textarea textarea-info mt-2' {...register("review", { required: true })} />
+                
+                {errors.review && <span>Write a review first</span>}
+
+                <input className='btn btn-primary mt-2' type="submit" />
+            </form>
         </div>
     );
 };
